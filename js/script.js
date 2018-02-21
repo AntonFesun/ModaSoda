04a99ee62c142d43d6ab9e06a82111815e006bc8
Andrew <karandriy@gmail.com>
2018-02-21 21:25:56 +0200

    added xhr, changed json file, add idea ide to ignore

--- js/script.js
+++ js/script.js
@@ -7,6 +7,27 @@ $('document').ready(function(){
 });
 
 function loadGoods() {
+    // по сути ты получаешь тоже самое что jQuery.getJSON но именно чистым JS
+
+    // 1. Создаём новый объект XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+
+    // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+    xhr.open('POST', 'glasses.json', false);
+
+    // 3. Отсылаем запрос
+    xhr.send();
+
+    // 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status !== 200)
+        console.error(xhr.responseText);
+    else
+        console.log(`type of response: ${typeof xhr.response} ${xhr.response}`);
+
+    var responeObj = JSON.parse(xhr.response);
+
+    console.log(responeObj);
+
     $.getJSON('glasses.json', function (data) {
         var out = '';
         for (var key in data) {
