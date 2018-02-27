e66ff1e8359ae5f5ac879a675edf5044310a9350
Andrew <karandriy@gmail.com>
2018-02-27 23:02:28 +0200

    Merge branch 'master' of https://github.com/AntonFesun/ModaSoda
    # Conflicts:
    #	js/script.js

--- js/script.js
+++ js/script.js
@@ -24,87 +24,87 @@ function loadGoods() {
     else
 //        console.log(`type of response: ${typeof xhr.response} ${xhr.response}`);
 
-    var base = JSON.parse(xhr.response),
-        doc = document,
-        loc = location.href;
-        console.log(typeof loc);
+        var base = JSON.parse(xhr.response),
+            doc = document,
+            loc = location.href;
+    console.log(typeof loc);
 
     var links = [];
 
-        for (var i = 0; i < base.length; i++) {
-            var wrapperForGlasses = doc.getElementsByClassName("items"),
-                elements = doc.createElement("a"),
-                head = doc.createElement("h5"),
-                price = doc.createElement("p"),
-                image = doc.createElement("img"),
-                description = doc.createElement("p"),
-                butt = doc.createElement("button");
+    for (var i = 0; i < base.length; i++) {
+        var wrapperForGlasses = doc.getElementsByClassName("items"),
+            elements = doc.createElement("a"),
+            head = doc.createElement("h5"),
+            price = doc.createElement("p"),
+            image = doc.createElement("img"),
+            description = doc.createElement("p"),
+            butt = doc.createElement("button");
 
-            function aceOfBase() {
+        function aceOfBase() {
 
-                    head.classList.add("items-glasses-name");
-                    price.classList.add("items-glasses-price");
-                    image.classList.add("items-glasses-image");
-                    description.classList.add("items-glasses-description");
-                    butt.classList.add("add-to-cart");
-                    elements.classList.add("items-glasses");
+            head.classList.add("items-glasses-name");
+            price.classList.add("items-glasses-price");
+            image.classList.add("items-glasses-image");
+            description.classList.add("items-glasses-description");
+            butt.classList.add("add-to-cart");
+            elements.classList.add("items-glasses");
 
-                    head.innerHTML = base[i].name;
-                    price.innerHTML = 'Ціна: ' + base[i].price;
-                    image.src = base[i].foto_1;
-                    description.innerHTML = 'Опис: ' + base[i].descript;
-                    elements.appendChild(head);
-                    elements.appendChild(price);
-                    elements.appendChild(image);
-                    elements.appendChild(description);
-                    wrapperForGlasses[0].appendChild(elements);
-                    console.log(elements);
-                    console.log(wrapperForGlasses);
+            head.innerHTML = base[i].name;
+            price.innerHTML = 'Ціна: ' + base[i].price;
+            image.src = base[i].foto_1;
+            description.innerHTML = 'Опис: ' + base[i].descript;
+            elements.appendChild(head);
+            elements.appendChild(price);
+            elements.appendChild(image);
+            elements.appendChild(description);
+            wrapperForGlasses[0].appendChild(elements);
+            console.log(elements);
+            console.log(wrapperForGlasses);
 
-            }
+        }
 
-            if (base[i].type === 'оправа' && loc === 'http://192.168.0.109/other/rims.html' ) {
-                aceOfBase();
-            } else if (base[i].type === 'сонцезахисні' && loc === '../other/all_sunglasses.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Alexander McQueen' && loc === '../designer/alexanderMcQueen.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Blumarine' && loc === '../designer/blumarine.html') {
-                aceOfBase();
-            }  else if (base[i].name === 'Bottega Veneta' && loc === '../designer/bottegaVeneta.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Carolina Herrera' && loc === '../designer/carolinaHerrera.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Chopard' && loc === '../designer/chopard.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Dunhill' && loc === '../designer/dunhill.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Escada' && loc === '../designer/escada.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Furla' && loc === '../designer/furla.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Lanvin' && loc === '../designer/lanvin.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Momodesign' && loc === '../designer/momoDesign.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Roberto Cavalli' && loc === '../designer/robertoCavalli.html') {
-                aceOfBase();
-            } else if (base[i].name == 'Tom Ford' && loc === '../designer/tomFord.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Trussardi' && loc === '../designer/trussardi.html') {
-                aceOfBase();
-            } else if (base[i].name === 'Zegna' && loc === '../designer/zegna.html') {
-                aceOfBase();
-            } else if (base[i].sex === 'чоловічі' && loc === '../designer/men.html') {
-                aceOfBase();
-            } else if (base[i].sex === 'жіночі' && loc === '../designer/women.html') {
-                aceOfBase();
-            }
-        };
+        if (base[i].type === 'оправа' && loc === 'http://192.168.0.109/other/rims.html') {
+            aceOfBase();
+        } else if (base[i].type === 'сонцезахисні' && loc === '../other/all_sunglasses.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Alexander McQueen' && loc === '../designer/alexanderMcQueen.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Blumarine' && loc === '../designer/blumarine.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Bottega Veneta' && loc === '../designer/bottegaVeneta.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Carolina Herrera' && loc === '../designer/carolinaHerrera.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Chopard' && loc === '../designer/chopard.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Dunhill' && loc === '../designer/dunhill.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Escada' && loc === '../designer/escada.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Furla' && loc === '../designer/furla.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Lanvin' && loc === '../designer/lanvin.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Momodesign' && loc === '../designer/momoDesign.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Roberto Cavalli' && loc === '../designer/robertoCavalli.html') {
+            aceOfBase();
+        } else if (base[i].name == 'Tom Ford' && loc === '../designer/tomFord.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Trussardi' && loc === '../designer/trussardi.html') {
+            aceOfBase();
+        } else if (base[i].name === 'Zegna' && loc === '../designer/zegna.html') {
+            aceOfBase();
+        } else if (base[i].sex === 'чоловічі' && loc === '../designer/men.html') {
+            aceOfBase();
+        } else if (base[i].sex === 'жіночі' && loc === '../designer/women.html') {
+            aceOfBase();
+        }
+    }
+    ;
 };
 loadGoods();
 
-    
 
 //function addToCart() {
 //    //добавляем товар в корзину
