dc50466b7635385c907686642be91fa88a89df8d
Andrew <karandriy@gmail.com>
2018-02-27 23:50:38 +0200

    added types

--- js/script.js
+++ js/script.js
@@ -6,6 +6,39 @@ var cart = {}; //моя корзина
 //    showMiniCart();
 //});
 
+function aceOfBase(obj) {
+    if (doc.querySelector('#jsonItems')) {
+        var wrapperForGlasses = doc.querySelector("#jsonItems"),
+            elements = doc.createElement("a"),
+            head = doc.createElement("h5"),
+            price = doc.createElement("p"),
+            image = doc.createElement("img"),
+            description = doc.createElement("p"),
+            butt = doc.createElement("button");
+
+        head.classList.add("items-glasses-name");
+        price.classList.add("items-glasses-price");
+        image.classList.add("items-glasses-image");
+        description.classList.add("items-glasses-description");
+        butt.classList.add("add-to-cart");
+        elements.classList.add("items-glasses");
+
+        head.innerHTML = obj.name;
+        price.innerHTML = 'Ціна: ' + obj.price;
+        image.src = obj.foto_1;
+        description.innerHTML = 'Опис: ' + obj.descript;
+        elements.appendChild(head);
+        elements.appendChild(price);
+        elements.appendChild(image);
+        elements.appendChild(description);
+        wrapperForGlasses.appendChild(elements);
+        console.log(elements);
+        console.log(wrapperForGlasses);
+    }
+
+}
+
+
 function loadGoods() {
     // по сути ты получаешь тоже самое что jQuery.getJSON но именно чистым JS
 
@@ -31,76 +64,36 @@ function loadGoods() {
 
     var links = [];
 
-    for (var i = 0; i < base.length; i++) {
-        var wrapperForGlasses = doc.getElementsByClassName("items"),
-            elements = doc.createElement("a"),
-            head = doc.createElement("h5"),
-            price = doc.createElement("p"),
-            image = doc.createElement("img"),
-            description = doc.createElement("p"),
-            butt = doc.createElement("button");
+    if (doc.querySelector('#jsonItems')) {
+        var items = doc.querySelector('#jsonItems');
 
-        function aceOfBase() {
+        for (var i = 0; i < base.length; i++) {
+            var isType = false,
+                isBrand = false,
+                isSex = false;
 
-            head.classList.add("items-glasses-name");
-            price.classList.add("items-glasses-price");
-            image.classList.add("items-glasses-image");
-            description.classList.add("items-glasses-description");
-            butt.classList.add("add-to-cart");
-            elements.classList.add("items-glasses");
+            if (items.getAttribute('data-type').length > 0 && items.getAttribute('data-type') === base[i].type)
+                isType = true;
+            else if (items.getAttribute('data-type').length === 0)
+                isType = true;
+            // if (!isType) continue;
 
-            head.innerHTML = base[i].name;
-            price.innerHTML = 'Ціна: ' + base[i].price;
-            image.src = base[i].foto_1;
-            description.innerHTML = 'Опис: ' + base[i].descript;
-            elements.appendChild(head);
-            elements.appendChild(price);
-            elements.appendChild(image);
-            elements.appendChild(description);
-            wrapperForGlasses[0].appendChild(elements);
-            console.log(elements);
-            console.log(wrapperForGlasses);
 
-        }
+            if (items.getAttribute('data-name').length > 0 && items.getAttribute('data-name') === base[i].name)
+                isBrand = true;
+            else if (items.getAttribute('data-name').length === 0)
+                isBrand = true;
 
-        if (base[i].type === 'оправа' && loc === 'http://192.168.0.109/other/rims.html') {
-            aceOfBase();
-        } else if (base[i].type === 'сонцезахисні' && loc === '../other/all_sunglasses.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Alexander McQueen' && loc === '../designer/alexanderMcQueen.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Blumarine' && loc === '../designer/blumarine.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Bottega Veneta' && loc === '../designer/bottegaVeneta.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Carolina Herrera' && loc === '../designer/carolinaHerrera.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Chopard' && loc === '../designer/chopard.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Dunhill' && loc === '../designer/dunhill.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Escada' && loc === '../designer/escada.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Furla' && loc === '../designer/furla.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Lanvin' && loc === '../designer/lanvin.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Momodesign' && loc === '../designer/momoDesign.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Roberto Cavalli' && loc === '../designer/robertoCavalli.html') {
-            aceOfBase();
-        } else if (base[i].name == 'Tom Ford' && loc === '../designer/tomFord.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Trussardi' && loc === '../designer/trussardi.html') {
-            aceOfBase();
-        } else if (base[i].name === 'Zegna' && loc === '../designer/zegna.html') {
-            aceOfBase();
-        } else if (base[i].sex === 'чоловічі' && loc === '../designer/men.html') {
-            aceOfBase();
-        } else if (base[i].sex === 'жіночі' && loc === '../designer/women.html') {
-            aceOfBase();
+
+
+
+
+            if (isType && isBrand && isSex)
+                aceOfBase(base[i]);
+
         }
     }
+
     ;
 };
 loadGoods();
